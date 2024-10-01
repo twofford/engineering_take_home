bf43b199608e36e791d36724445427c8e48c7793
Jon Moniaci <jon.moniaci@perchwell.com>
2024-10-01 13:32:12 -0400

    Tidy up db config, add rspec boilerplate, remove README instructions, remove turbo/stimuls and boilerplate

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
